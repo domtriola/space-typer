55d52cf31feeb40186b0d08a469d50be62bcbe3b
Dominick Triola <dominick@ignovadesign.com>
2017-01-11 09:58:28 -0800

    update usermenu so it redirects when unmounting

--- frontend/components/header/user_menu.jsx
+++ frontend/components/header/user_menu.jsx
@@ -4,23 +4,19 @@ import { Link } from 'react-router';
 class UserMenu extends React.Component {
   constructor(props) {
     super(props);
-
-    this.logout = this.logout.bind(this);
   }
 
   componentWillUpdate() {
     this.redirectUnlessLoggedIn();
   }
 
+  componentWillUnmount() {
+    this.props.router.push("/login");
+  }
+
   redirectUnlessLoggedIn() {
     if (!this.props.currentUser)
       this.props.router.push("/login");
-  }
-
-  // TODO: this isn't working, figure out how to redirect on logout
-  logout() {
-    let router = this.props.router;
-    this.props.logout().then(router.push("/login"));
   }
 
   render() {
@@ -31,7 +27,7 @@ class UserMenu extends React.Component {
           <li>
             <Link to={`users/${this.props.currentUser.id}`}>Profile</Link>
           </li>
-          <li><button onClick={this.logout}>Logout</button></li>
+          <li><button onClick={this.props.logout}>Logout</button></li>
         </ul>
       </div>
     );
--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => (
-  <div>Home</div>
+  <div className="container">Home</div>
 );
 
 export default Home;
