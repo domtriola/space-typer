cccd5fc5a3b22f8058243da6651213a00da63b3d
Dominick Triola <dominick@ignovadesign.com>
2017-01-11 13:18:00 -0800

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,19 @@ q1 = Quote.create!(
   url: "https://www.amazon.com/gp/product/0345418778/ref=as_li_tl?ie=UTF8&tag=spacetyper-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=0345418778&linkId=87f73cd3179466e6a2488b81584172f8",
   image_url: "http://ecx.images-amazon.com/images/I/51aRCqbDyVL.jpg")
 
+q2 = Quote.create!(
+  title: "Mostly Harmless",
+  body: "Everybody has their moment of great opportunity in life. If you happen to miss the one you care about, then everything else in life becomes eerily easy.",
+  url: "https://www.amazon.com/gp/product/0345418778/ref=as_li_tl?ie=UTF8&tag=spacetyper-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=0345418778&linkId=87f73cd3179466e6a2488b81584172f8",
+  image_url: "http://ecx.images-amazon.com/images/I/51aRCqbDyVL.jpg")
+
+q3 = Quote.create!(
+  title: "Mostly Harmless",
+  body: "One of the extraordinary things about life is the sort of places it's prepared to put up with living.",
+  url: "https://www.amazon.com/gp/product/0345418778/ref=as_li_tl?ie=UTF8&tag=spacetyper-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=0345418778&linkId=87f73cd3179466e6a2488b81584172f8",
+  image_url: "http://ecx.images-amazon.com/images/I/51aRCqbDyVL.jpg")
+
 Score.create!(wpm: 56, user_id: u1.id, quote_id: q1.id)
+Score.create!(wpm: 52, user_id: u1.id, quote_id: q1.id)
+Score.create!(wpm: 48, user_id: u1.id, quote_id: q2.id)
+Score.create!(wpm: 50, user_id: u1.id, quote_id: q3.id)
