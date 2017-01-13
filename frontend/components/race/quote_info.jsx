3c6e3b261590b56cff88089d62036b0cf0a33cbd
Dominick Triola <dominick@ignovadesign.com>
2017-01-12 16:41:18 -0800

    change quote info copy

--- frontend/components/race/quote_info.jsx
+++ frontend/components/race/quote_info.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const QuoteInfo = ({ quote }) => (
   <div className="quote-info">
-    <h3>That was a quote from:</h3>
+    <h3>This quote is from:</h3>
     <a href={quote.url} target="_blank">{quote.title}</a>
     <a href={quote.url} target="_blank">
       <img src={quote.image_url} />
