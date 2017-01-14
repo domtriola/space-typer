f432a35502b648dd2b1e82b4634f394685dc8fa4
Dominick Triola <dominick@ignovadesign.com>
2017-01-13 16:38:53 -0800

    limit quote user scores and order by date

--- app/controllers/api/quotes_controller.rb
+++ app/controllers/api/quotes_controller.rb
@@ -2,6 +2,7 @@ class Api::QuotesController < ApplicationController
   def quote
     @quote = Quote.order("RANDOM()").first
     @scores = @quote.scores.where(user_id: current_user.id)
+      .order('created_at DESC').limit(5)
     @high_scores = @quote.scores.order('wpm DESC').limit(10)
 
     render :show
