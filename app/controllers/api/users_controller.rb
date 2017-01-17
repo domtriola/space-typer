5f7626cf5aed3154c056ca3753a71356e9c6156f
Dominick Triola <dominick@ignovadesign.com>
2017-01-17 14:55:44 -0800

    log in user on creation

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -62,6 +62,7 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login_user!(@user)
       render :show
     else
       render json: @user.errors.full_messages, status: 422
