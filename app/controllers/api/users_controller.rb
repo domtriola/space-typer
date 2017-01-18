3524ac48a924b27f907226567dc7d9f971b4cced
Dominick Triola <dominick@ignovadesign.com>
2017-01-18 11:18:16 -0800

    refactor user stat query

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -13,17 +13,6 @@ class Api::UsersController < ApplicationController
           scores.user_id = ?
       SQL
 
-      total_won = Score.find_by_sql([<<-SQL, user])
-        SELECT
-          COUNT(*) AS total_won,
-          AVG(scores.wpm) AS avg_wpm,
-          MAX(scores.wpm) AS max_wpm
-        FROM
-          scores
-        WHERE
-          scores.user_id = ? AND scores.won = TRUE
-      SQL
-
       avg_last_ten = Score.find_by_sql([<<-SQL, user])
         SELECT
           AVG(last_ten.wpm) AS avg
@@ -41,8 +30,8 @@ class Api::UsersController < ApplicationController
       SQL
 
       user_stats = user_stats.first
-      total_won = total_won.first.total_won
       avg_last_ten = avg_last_ten.first.avg
+      total_won = Score.where(user_id: user.id, won: true).count
 
       @stats = {
         username: user.username,
